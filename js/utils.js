85118ba6cadc59e086e77301580e95d59bdf4b5e
Lana Kostina <exxlain@gmail.com>
2018-06-01 22:42:49 +0700

    разбила на модули

---
+++ js/utils.js
@@ -0,0 +1,12 @@
+const mainElement = document.querySelector(`main.central`);
+
+export const getElementFromTemplate = (template) => {
+  const container = document.createElement(`div`);
+  container.innerHTML = template;
+  return container;
+};
+
+export const changeScreen = (element) => {
+  mainElement.innerHTML = ``;
+  mainElement.appendChild(element);
+};
