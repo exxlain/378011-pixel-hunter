ce0d38144f76f824ff6e158bc0eec971a16b0dea
Lana Kostina <exxlain@gmail.com>
2018-06-15 23:46:02 +0700

    вынесла форму игры в отдельный модуль

--- js/views/level-view.js
+++ js/views/level-view.js
@@ -1,9 +1,10 @@
 import {quests} from '../data/quest-data';
 import {GameType} from '../data/data';
 import AbstractView from './abstract-view';
+import headerGameTemplate from '../blocks/header-game';
+import gameFormTemplate from '../blocks/game-form';
 import statsTemplate from '../blocks/stats';
 import footerTemplate from '../blocks/footer';
-import headerGameTemplate from '../blocks/header-game';
 
 const getLevel = (level) => quests[`game-${level}`];
 
@@ -16,76 +17,7 @@ export default class LevelView extends AbstractView {
   }
 
   get template() {
-    switch (this._level.gameType) {
-      case GameType.PHOTO_OR_PICTURE_TWO:
-        return `${headerGameTemplate(this.state)}<div class="game">
-    <p class="game__task">${this._level.description}</p>
-    <form class="game__content">
-      <div class="game__option">
-        <img src="${this._level.questions[0].image}" alt="Option 1" width="468" height="458">
-        <label class="game__answer game__answer--photo">
-          <input name="question1" type="radio" value="photo">
-          <span>Фото</span>
-        </label>
-        <label class="game__answer game__answer--paint">
-          <input name="question1" type="radio" value="paint">
-          <span>Рисунок</span>
-        </label>
-      </div>
-      <div class="game__option">
-        <img src="${this._level.questions[1].image}" alt="Option 2" width="468" height="458">
-        <label class="game__answer  game__answer--photo">
-          <input name="question2" type="radio" value="photo">
-          <span>Фото</span>
-        </label>
-        <label class="game__answer  game__answer--paint">
-          <input name="question2" type="radio" value="paint">
-          <span>Рисунок</span>
-        </label>
-      </div>
-    </form>
-    <div class="stats">
-    </div>
-  </div>${statsTemplate(this.answers)}${footerTemplate}`;
-      case GameType.PHOTO_OR_PICTURE_ONE:
-        return `${headerGameTemplate(this.state)}<div class="game">
-    <p class="game__task">${this._level.description}</p>
-    <form class="game__content  game__content--wide">
-      <div class="game__option">
-        <img src="${this._level.questions.image}" alt="Option 1" width="705" height="455">
-        <label class="game__answer  game__answer--photo">
-          <input name="question1" type="radio" value="photo">
-          <span>Фото</span>
-        </label>
-        <label class="game__answer  game__answer--wide  game__answer--paint">
-          <input name="question1" type="radio" value="paint">
-          <span>Рисунок</span>
-        </label>
-      </div>
-    </form>
-    <div class="stats">
-    </div>
-  </div>${statsTemplate(this.answers)}${footerTemplate}`;
-      case GameType.FIND_ONE:
-        return `${headerGameTemplate(this.state)}<div class="game">
-    <p class="game__task">${this._level.description}</p>
-    <form class="game__content  game__content--triple">
-      <div class="game__option">
-        <img src="${this._level.questions[0].image}" alt="Option 1" width="304" height="455">
-      </div>
-      <div class="game__option  game__option--selected">
-        <img src="${this._level.questions[1].image}" alt="Option 2" width="304" height="455">
-      </div>
-      <div class="game__option">
-        <img src="${this._level.questions[2].image}" alt="Option 3" width="304" height="455">
-      </div>
-    </form>
-    <div class="stats">
-    </div>
-  </div>${statsTemplate(this.answers)}${footerTemplate}`;
-      default:
-        throw new RangeError(`No such type of game`);
-    }
+    return `${headerGameTemplate(this.state)}${gameFormTemplate(this._level)}${statsTemplate(this.answers)}${footerTemplate}`;
   }
 
   onAnswer(answer) {
